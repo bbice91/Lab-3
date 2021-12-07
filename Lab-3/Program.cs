5acbdf24d0ab46ea05c3a0329662cc31236e9a5a
bbice91 <30932991+bbice91@users.noreply.github.com>
2021-12-06 19:38:11 -0500

    Lab-3 Submission

--- Lab-3/Program.cs
+++ Lab-3/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Input;
+            string Decision;
+            string Name;
+            Console.WriteLine("Greetings! What is your name?");
+            Name = Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Name}, enter a number between 1 and 100:");
+                Input = Convert.ToInt32(Console.ReadLine());
+                if (Input % 2 != 0)
+                {
+                    if (Input > 60)
+                    {
+                        Console.WriteLine($"{Name}, {Input} and Odd");
+                    }
+                }
+                else if (2 <= Input && Input <= 25)
+                {
+                    Console.WriteLine($"{Name}, Even and less than 25");
+                }
+                else if (Input is >= 26 and <= 60)
+                {
+                    Console.WriteLine($"{Name}, Even");
+                }
+                else if (Input > 60)
+                {
+                    Console.WriteLine($"{Name}, {Input} and Even");
+                }
+
+                Console.WriteLine($"Continue {Name}? (y/n)");
+                Decision = Console.ReadLine().ToLower();
+                if (Decision == "n")
+                {
+                    Console.WriteLine($"Bye {Name}!");
+                    Environment.Exit(0);
+                }
+
+            }
+
+        }
+
+    }
+
+}
